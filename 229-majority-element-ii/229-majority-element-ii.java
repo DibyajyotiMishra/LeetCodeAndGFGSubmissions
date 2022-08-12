0b42793875e816a41c434a0e2fe475165b16e02d
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-08-13 01:35:36 +0530

    Time: 10 ms (36.01%), Space: 52.4 MB (5.78%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,14 +1,52 @@
+// Using HashMap to store the frequency of each number.
+
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//        List<Integer> majorityElements = new ArrayList<>();
+//         int n = nums.length;
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int num: nums)
+//             map.put(num, map.getOrDefault(num, 0)+1);
+//         for(Map.Entry<Integer, Integer> e : map.entrySet()){
+//             if(e.getValue() > n/3)
+//                 majorityElements.add(e.getKey());
+//         }
+//         return majorityElements;
+//     }
+// }
+    
+// Using Bayer-Moore Majority Vote Algo
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-       List<Integer> majorityElements = new ArrayList<>();
-        int n = nums.length;
-       HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num: nums)
-            map.put(num, map.getOrDefault(num, 0)+1);
-        for(Map.Entry<Integer, Integer> e : map.entrySet()){
-            if(e.getValue() > n/3)
-                majorityElements.add(e.getKey());
+        Integer major1 = null, major2 = null, cnt1 = 0, cnt2 = 0;
+        for (Integer num : nums) {
+            if (num.equals(major1)) {
+                cnt1++;
+            } else if (num.equals(major2)) {
+                cnt2++;
+            } else if (cnt1 == 0) {
+                major1 = num;
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                major2 = num;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
         }
-        return majorityElements;
+        
+        cnt1 = cnt2 = 0;
+        for (Integer num : nums) {
+            if (num.equals(major1)) cnt1++;
+            else if (num.equals(major2)) cnt2++;
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        if (cnt1 > nums.length / 3) result.add(major1);
+        if (cnt2 > nums.length / 3) result.add(major2);
+        return result;
+    
     }
 }
