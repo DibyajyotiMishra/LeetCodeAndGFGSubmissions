d93fc50015265f3d8f67e7c5010ee644affd44c2
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-08-31 00:36:08 +0530

    Time: 4 ms (80.39%), Space: 43.1 MB (43.37%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -1,31 +1,28 @@
 class Solution {
+
     public String removeDuplicateLetters(String s) {
-        int len = s.length(); // length of the string.
-        
+        int len = s.length(); // length of the string
+
         boolean visited[] = new boolean[26];
         int lastIndex[] = new int[26];
-        
-        for(int i = 0; i < len; i++){
-            lastIndex[s.charAt(i) - 'a'] = i; // find the last index of each char.
-        }
-        
+
+        for (int i = 0; i < len; i++) lastIndex[s.charAt(i) - 'a'] = i; // marking the last occurance (index) of each character
+
         Stack<Integer> st = new Stack<>();
-        for(int i = 0; i < len; i++){
-            int c = s.charAt(i) - 'a';
-            
-            if(visited[c]) continue; // skip over duplicate elements.
-            
-            visited[c] = true;
-            
-            while(!st.isEmpty() && st.peek() > c && lastIndex[st.peek()] > i)
-                visited[st.pop()] = false;
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            int c = s.charAt(i) - 'a'; // the value of a particular char in terms of index like a = 0, b = 1, c = 2, and so on.
+
+            if (visited[c]) continue; // skip over duplicates.
+
+            visited[c] = true; // mark an character as visited.
+
+            while (!st.isEmpty() && st.peek() > c && lastIndex[st.peek()] > i) visited[st.pop()] = false;
             st.push(c);
         }
-        
-        StringBuilder ans = new StringBuilder();
-        for(int i : st)
-            ans.append((char)(i+'a'));
-            
+
+        for (int ind : st) ans.append((char) (ind + 'a')); // convert index back into char.
+
         return ans.toString();
     }
 }
