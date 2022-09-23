e896bc5faa2701c00677fb4f13a0d0be4974a327
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-09-23 17:58:54 +0530

    Time: 141 ms (13.30%), Space: 78.9 MB (10.57%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,14 +1,24 @@
+class HeapComparator implements Comparator<Integer>{
+    @Override
+    public int compare(Integer num1, Integer num2){
+        int res = num1.compareTo(num2);
+        if(res > 0)
+            return 1;
+        else if(res < 0)
+            return -1;
+        else return 0;
+    }
+}
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new HeapComparator());
         
         for(int num: nums){
             pq.add(num);
+            while(pq.size() > k)
+                pq.poll();
         }
         
-        while(k-- > 1)
-            pq.poll();
-        
-        return pq.peek();
+        return pq.poll();
     }
 }
