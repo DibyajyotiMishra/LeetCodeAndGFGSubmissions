a1e97d9bab73e871f68778f73c56f4af92c3c187
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-09-23 19:20:42 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (37.30%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,31 +1,20 @@
 class Solution {
-
-    public void sortColors(int[] nums) {
-        int start = 0, end = nums.length - 1, mid = 0, temp = 0;
-
-        // loop as long as mid != to end.
-        // Solve by Dutch National Flag approach
-        // to perform sorting
-
-        while (mid <= end) {
-            switch (nums[mid]) {
+    public void sortColors(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1, temp = 0;
+        while(mid <= high){
+            switch(arr[mid]){
                 case 0:
-                    temp = nums[start];
-                    nums[start] = nums[mid];
-                    nums[mid] = temp;
-                    mid++;
-                    start++;
+                    temp = arr[low];
+                    arr[low++] = arr[mid];
+                    arr[mid++] = temp;
                     break;
                 case 1:
                     mid++;
                     break;
                 case 2:
-                    temp = nums[mid];
-                    nums[mid] = nums[end];
-                    nums[end] = temp;
-                    end--;
-                    break;
-                default:
+                    temp = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high--] = temp;
                     break;
             }
         }
