55ca6d181bedede34a9d63b4ddf71caddc776bdf
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-12-20 15:17:06 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (75.65%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int totalSum = (n * (n+1)) >> 1;
+        int arraySum = 0;
+        for(int num: nums)
+            arraySum += num;
+        return totalSum - arraySum;
+    }
+}
