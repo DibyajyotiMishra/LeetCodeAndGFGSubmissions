e3740778a803075be43c7e499c0cc4f635136bc5
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-12-22 12:06:43 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (83.13%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while(mid <= high) {
+            switch(nums[mid]) {
+                case 0: 
+                    int temp = nums[low];
+                    nums[low++] = nums[mid];
+                    nums[mid++] = temp;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    temp = nums[high];
+                    nums[high--] = nums[mid];
+                    nums[mid] = temp;
+                    break;
+            }
+        }
+    }
+}
