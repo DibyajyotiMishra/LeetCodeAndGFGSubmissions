d889b3ee286e135d1512879735ef688de057af3e
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-12-30 00:10:58 +0530

    Time: 13 ms (94.91%), Space: 41.8 MB (85.83%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,17 +1,15 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int n = nums.length, i = 0;
-        int minDistance = Integer.MAX_VALUE, ans = -1;
-        for(; i < n-2; i++) {
+        int n = nums.length, minDistance = Integer.MAX_VALUE, ans = -1;
+        for(int i = 0; i < n-2; i++) {
             int j = i+1, k = n-1;
             while(j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if(Math.abs(target-sum) < minDistance) {
                     minDistance = Math.abs(target-sum);
                     ans = sum;
-                    // j++; k--;
-                } 
+                }
                 if(target-sum == 0)
                     return sum;
                 else if(target < sum)
@@ -19,6 +17,7 @@ class Solution {
                 else j++;
             }
         }
+        
         return ans;
     }
 }
