7d4bdf19295c052d99a8de285f147da8c85a8b5d
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-12-30 19:24:21 +0530

    Time: 17 ms (81.87%), Space: 43.3 MB (70.37%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,53 +1,55 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        List<List<Integer>> ans= new ArrayList<>();
-        for(int a =0; a<nums.length-3;a++){
-           if(a==0 || (a>0 && nums[a]!= nums[a-1] )){
-            List<List<Integer>> sol = threeSum( nums,target-nums[a],a+1);
-            for(int i =0;i<sol.size() ;i++){
-                sol.get(i).add(nums[a]);
-                ans.add(sol.get(i));
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length-3; i++) {
+            if(i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                List<List<Integer>> list = threeSum(nums, target-nums[i], i+1);
+                for(int itr = 0; itr < list.size(); itr++) {
+                    list.get(itr).add(nums[i]);
+                    ans.add(list.get(itr));
+                }
             }
-           }
-           
-           
         }
-        return ans;
-    }
-    public List<List<Integer>> threeSum(int[] nums, long target,int firstIndex) {
         
-        List<List<Integer>> ans= new ArrayList<>();
-        for(int a =firstIndex; a<nums.length-2;a++){
-            if(a==firstIndex || (a>0 && nums[a]!= nums[a-1] )){
-            List<List<Integer>> sol = twoSum( nums,target-nums[a],a+1);
-            for(int i =0;i<sol.size() ;i++){
-                sol.get(i).add(nums[a]);
-                ans.add(sol.get(i));
-            }
-            }
-        }
         return ans;
     }
-
-    public List<List<Integer>> twoSum(int[] nums,long target, int firstIndex){
-         List<List<Integer>> ans= new ArrayList<>();
-         int low = firstIndex;
-         int high = nums.length-1;
-         while(low < high){
-             long sum = nums[low]+nums[high];
-             if( sum> target) high--;
-             else if(sum < target) low++;
-             else {
-                ans.add(new ArrayList<>( Arrays.asList(nums[low],nums[high])));
-                while(low<high && nums[low] == nums[low+1]) low++;
-                while(low<high && nums[high] == nums[high-1]) high--;
-                low++;
-                high--;
-             }
-
-         }
-         return ans;
+    
+    
+    public List<List<Integer>> threeSum(int[] nums, long target, int ind) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = ind; i < nums.length-2; i++) {
+            if(i == ind || (i > 0 && nums[i] != nums[i-1])) {
+                List<List<Integer>> list = twoSum(nums, target-nums[i], i+1);
+                for(int itr = 0; itr < list.size(); itr++) {
+                    list.get(itr).add(nums[i]);
+                    ans.add(list.get(itr));
+                }
+            }
+        }
+        
+        return ans;
     }
-
+    
+    
+    public List<List<Integer>> twoSum(int[] nums, long target, int ind) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int low = ind, high = nums.length-1;
+        while(low < high) {
+            int sum = nums[low] + nums[high];
+            if(sum > target) {
+                high--;
+            } else if(sum < target) {
+                low++;
+            } else {
+                ans.add(new ArrayList<>(Arrays.asList(nums[low], nums[high])));
+                
+                while(low < high && nums[low] == nums[low+1]) low++;
+                while(low < high && nums[high] == nums[high-1]) high--;
+                low++; high--;
+            }
+        }
+        
+        return ans;
+    }
 }
