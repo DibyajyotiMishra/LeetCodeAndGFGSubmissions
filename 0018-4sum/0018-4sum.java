82a7c0308181ed007d7e4e92cb2cdcdafe07d1e4
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-12-30 20:59:23 +0530

    Time: 16 ms (82.64%), Space: 43 MB (76.46%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i < nums.length-3; i++) {
+        for(int i = 0; i < nums.length - 3; i++) {
             if(i == 0 || (i > 0 && nums[i] != nums[i-1])) {
                 List<List<Integer>> list = threeSum(nums, target-nums[i], i+1);
                 for(int itr = 0; itr < list.size(); itr++) {
@@ -16,8 +16,9 @@ class Solution {
     }
     
     
-    public List<List<Integer>> threeSum(int[] nums, long target, int ind) {
+    public List<List<Integer>> threeSum(int nums[], long target, int ind) {
         List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
         for(int i = ind; i < nums.length-2; i++) {
             if(i == ind || (i > 0 && nums[i] != nums[i-1])) {
                 List<List<Integer>> list = twoSum(nums, target-nums[i], i+1);
@@ -32,21 +33,24 @@ class Solution {
     }
     
     
-    public List<List<Integer>> twoSum(int[] nums, long target, int ind) {
+    public List<List<Integer>> twoSum(int nums[], long target, int ind) {
+        int n = nums.length;
+        int lo = ind, hi = nums.length-1;
         List<List<Integer>> ans = new ArrayList<>();
-        int low = ind, high = nums.length-1;
-        while(low < high) {
-            int sum = nums[low] + nums[high];
-            if(sum > target) {
-                high--;
-            } else if(sum < target) {
-                low++;
-            } else {
-                ans.add(new ArrayList<>(Arrays.asList(nums[low], nums[high])));
-                
-                while(low < high && nums[low] == nums[low+1]) low++;
-                while(low < high && nums[high] == nums[high-1]) high--;
-                low++; high--;
+        while(lo < hi) {
+            long curSum = nums[lo] + nums[hi];
+            
+            if(curSum > target){
+                hi--;
+            }
+            else if(curSum < target) {
+                lo++;
+            }
+            else {
+                ans.add(new ArrayList<>(Arrays.asList(nums[lo], nums[hi])));
+                while(lo < hi && nums[lo] == nums[lo+1]) lo++;
+                while(lo < hi && nums[hi] == nums[hi-1]) hi--;
+                lo++; hi--;
             }
         }
         
