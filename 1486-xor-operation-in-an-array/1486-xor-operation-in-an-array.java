5a947fc03dae4d82db89e3e3d0ada8e1b9037f17
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2023-01-11 09:41:36 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (77.33%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -1,9 +1,25 @@
 class Solution {
     public int xorOperation(int n, int start) {
-        int xor = 0;
-        for(int i = 0; i < n; i++) {
-            xor ^= (start + (2 * i)); 
-        }
-        return xor;
+        int startWithoutFirstBit = start >> 1;
+        int from = startWithoutFirstBit;
+        int to = from + n - 1;
+        int xor = xorOfRange(from, to);
+        int answer = xor << 1;
+        if((start % 2 == 1) && (n % 2) == 1)
+            answer |= 1;
+        return answer;
+    }
+    
+    
+    private static int xorOfRange(int from, int to) {
+        return xorOfWholeNumbers(from-1) ^ xorOfWholeNumbers(to);
+    } 
+    
+    private static int xorOfWholeNumbers(int num) {
+        if(num % 4 == 0) return num;
+        else if(num % 4 == 1) return 1;
+        else if(num % 4 == 2) return (num+1);
+        else if(num % 4 == 3) return 0;
+        return 0;
     }
 }
