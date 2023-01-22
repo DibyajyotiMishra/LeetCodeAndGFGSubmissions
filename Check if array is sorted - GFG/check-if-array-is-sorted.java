430b7684a5cf9f1a811b5fbe84055802fb98584e
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2023-01-22 19:15:03 +0530

    Added solution - LeetHub

--- Check if array is sorted - GFG/check-if-array-is-sorted.java
+++ Check if array is sorted - GFG/check-if-array-is-sorted.java
@@ -32,10 +32,10 @@ import java.io.*;
 class Solution {
     boolean arraySortedOrNot(int[] arr, int n) {
         // code here
-        for(int i = 1; i < n; i++)
+        for(int i = 1; i < n; i++) {
             if(arr[i-1] > arr[i])
                 return false;
-                
+        }
         return true;
     }
 }
