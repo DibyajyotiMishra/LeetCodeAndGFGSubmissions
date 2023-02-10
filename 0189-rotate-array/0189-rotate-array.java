ff3202eb422ebf70a7a17ef32238edad1a5727bb
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2023-02-10 20:09:12 +0530

    Time: 0 ms (100.00%), Space: 58.2 MB (66.36%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,12 +2,12 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k %= n;
-        reverseTill(nums, 0, n-k-1);
-        reverseTill(nums, n-k, n-1);
-        reverseTill(nums, 0, n-1);
+        reverse(nums, 0, n-k-1);
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-1);
     }
     
-    private void reverseTill(int nums[], int st, int en) {
+    private void reverse(int nums[], int st, int en) {
         while(st <= en) {
             int temp = nums[st];
             nums[st++] = nums[en];
@@ -15,12 +15,3 @@ class Solution {
         }
     }
 }
-
-
-// n : 7, k : 3
-
-// 1 2 3 4 5 6 7
-
-// 4 3 2 1 7 6 5
-
-// 5 6 7 1 2 3 4
