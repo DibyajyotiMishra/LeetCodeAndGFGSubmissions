2ffb1f8f4b956010be38b95cd30bce6e2a5f02b0
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2023-02-13 23:15:12 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (73.38%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int num = 0;
-        for(int i = 0; i < nums.length; i++) 
-            num ^= nums[i];
-        return num;
+        int curXor = 0;
+        for(int num : nums)
+            curXor ^= num;
+        return curXor;
     }
 }
