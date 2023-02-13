f24a66a6a755990ad858cea2137e1001cd453922
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2023-02-13 19:52:38 +0530

    Time: 1 ms (100.00%), Space: 43.2 MB (97.20%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0, j = 0;
-        while(i < nums.length) {
+        int n = nums.length;
+        int zeroPos = 0;
+        for(int i = 0; i < nums.length; i++) {
             if(nums[i] != 0)
-                swap(nums, i, j++);
-            i++;
+                swap(nums, i, zeroPos++);
         }
     }
+    
     private void swap(int nums[], int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
