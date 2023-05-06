65a803ff866f2b8f56bef15e72123fae685dbd13
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2023-05-06 22:48:10 +0530

    Time: 1 ms (13.98%), Space: 41.1 MB (50.19%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,35 @@
+// Recursive Solution
+
+// class Solution {
+//     public double myPow(double x, int n) {
+//         if(n == 0)
+//             return 1;
+//         if(n < 0){
+//             return 1/x * myPow(1/x, -(n + 1));
+//         }
+//         return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+//     }
+// }
+
+
+//Iterative Solution
+
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long nn = n;
+        if(nn < 0) nn = -1 * nn;
+        while(nn > 0) {
+            if(nn % 2 == 1) {
+                ans = ans * x;
+                nn = nn - 1;
+            } else {
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
+        
+        if(n < 0) ans = (double)1.0 / (double)ans;
+        return ans;
+    }
+}
