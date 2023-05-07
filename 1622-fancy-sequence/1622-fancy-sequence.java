ed63b3e6dcfd351b372f98ed5e671861660dd4d5
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2023-05-08 00:26:20 +0530

    Time: 328 ms (19.30%), Space: 83.4 MB (71.93%) - LeetHub

---
+++ 1622-fancy-sequence/1622-fancy-sequence.java
@@ -0,0 +1,44 @@
+import java.math.BigInteger;
+class Fancy {
+    public static final BigInteger MOD = BigInteger.valueOf((int)(1e9 + 7));
+    private List<BigInteger> ans = new ArrayList<>();
+    private List<BigInteger> mult = new ArrayList<>();
+    private List<BigInteger> added = new ArrayList<>();
+    
+    public Fancy() {
+        mult.add(BigInteger.ONE);
+        added.add(BigInteger.ZERO);
+    }
+    
+    public void append(int val) {
+        ans.add(BigInteger.valueOf(val));
+        mult.add(mult.get(mult.size() - 1));
+        added.add(added.get(added.size() - 1));
+    }
+    
+    public void addAll(int inc) {
+        BigInteger last = added.get(added.size() - 1);
+        added.remove(added.size() - 1);
+        added.add(last.add(BigInteger.valueOf(inc)).mod(MOD));
+    }
+    
+    public void multAll(int m) {
+        BigInteger lastAdded = added.get(added.size() - 1);
+        BigInteger lastMultiplied = mult.get(mult.size() - 1);
+        added.remove(added.size() - 1);
+        added.add(lastAdded.multiply(BigInteger.valueOf(m)).mod(MOD));
+        mult.remove(mult.size() - 1);
+        mult.add(lastMultiplied.multiply(BigInteger.valueOf(m)).mod(MOD));
+    }
+    
+    public int getIndex(int idx) {
+        if (idx >= ans.size()) {
+            return -1;
+        }
+        BigInteger quotient = mult.get(mult.size() - 1);
+        BigInteger dividend = mult.get(idx);
+        BigInteger factor = quotient.multiply(dividend.modInverse(MOD)).mod(MOD);
+        BigInteger inc = added.get(added.size() - 1).subtract(added.get(idx).multiply(factor).mod(MOD));
+        return ans.get(idx).multiply(factor).mod(MOD).add(inc).mod(MOD).intValue();
+    }
+}
