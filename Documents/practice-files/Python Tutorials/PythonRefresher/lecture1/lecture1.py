4c0c9d6743d4405f92d50251baca7c3c462f6369
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2024-03-17 01:52:05 +0530

    feat: start newsletter app

---
+++ Documents/practice-files/Python Tutorials/PythonRefresher/lecture1/lecture1.py
@@ -0,0 +1,90 @@
+"""
+String Assignment
+"""
+
+# days = int(input("enter number of days left for your birthday: "))
+# print(round(days/7))
+
+
+"""
+List Assignment
+"""
+
+# zoo = ["dog", "cat", "lion", "tiger", "monkey"]
+#
+# zoo.append("rhino")
+# print(zoo)
+# zoo.pop(0)
+# print(zoo)
+# print(zoo[:3])
+
+"""
+if else elif Assignment
+"""
+
+# while True:
+#     score = int(input("Enter marks: "))
+#     if score >= 90 and score <= 100:
+#         print("A")
+#     elif score >= 80 and score <= 89:
+#         print("B")
+#     elif score >= 70 and score <= 79:
+#         print("C")
+#     elif score >= 60 and score <= 69:
+#         print("D")
+#     elif score >= 0 and score <= 59:
+#         print("F")
+
+"""
+Loops Assignment
+"""
+
+my_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+# for day in my_list:
+#     if day == "Monday":
+#         continue
+#     i = 0
+#     while i < 3:
+#         print(day)
+#         i += 1
+
+"""
+Dictionary Assignment
+"""
+
+# my_vehicle = {
+#     "model": "Ford",
+#     "make": "Explorer",
+#     "year": 2018,
+#     "mileage" : 40000
+# }
+#
+# print("==========================================")
+# for keys, vals in my_vehicle.items():
+#     print(f"{keys} : {vals}")
+# print("==========================================")
+# vehicle2 = my_vehicle.copy();
+# vehicle2["number_of_tires"] = 4
+# vehicle2.pop("mileage")
+#
+# print("\n")
+# print("==========================================")
+# for keys in vehicle2:
+#     print(keys)
+# print("==========================================")
+
+"""
+Function assignment
+"""
+
+
+# def create_dictionary(first_name, last_name, age):
+#     return {
+#         "firstName": first_name,
+#         "lastName": last_name,
+#         "age": age
+#     }
+#
+#
+# print(create_dictionary("Dibyajyoti", "Mishra", 23))
